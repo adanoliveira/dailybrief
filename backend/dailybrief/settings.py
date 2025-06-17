99246ffbc9c547b9f2afe83046e6d81b6df0b188
Adan Oliveira <7815447+adanoliveira@users.noreply.github.com>
2025-06-17 11:27:43 -0300

    Updating files

--- backend/dailybrief/settings.py
+++ backend/dailybrief/settings.py
@@ -111,10 +111,7 @@ INSTALLED_APPS = [
     'apps.content.processor',
     'apps.content.quality',
     'apps.content.summariser',
-<<<<<<< HEAD
-=======
     'apps.content.analyzer',
->>>>>>> main
 ]
 
 MIDDLEWARE = [
